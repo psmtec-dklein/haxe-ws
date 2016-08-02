32a2b94c03bc65cd7b41df7a911596a498e9d182
David Klein <david.klein@psmtec.de>
2016-08-02 11:36:03 +0200

    flash: remove eventlisteners needs more testing

--- src/haxe/net/impl/SocketFlash.hx
+++ src/haxe/net/impl/SocketFlash.hx
@@ -12,46 +12,60 @@ import flash.net.Socket;
 class SocketFlash extends Socket2 {
     private var impl: Socket;
 
+	// TODO (DK) debug stuff only, remove when testing is finished
+	private static var cnt = 0;
+	private var id = cnt++;
+	// /TODO (DK) debug stuff only, remove when testing is finished
+	
     public function new(host:String, port:Int, secure:Bool, debug:Bool = false) {
         super(host, port, debug);
 
-        //debug = true;
-
-        this.debug = debug;
-
         this.impl = secure ? new SecureSocket() : new Socket();
         this.impl.endian = Endian.BIG_ENDIAN;
-        this.impl.addEventListener(flash.events.Event.CONNECT, function(e:Event) {
-            if (debug) trace('SocketFlash.connect');
-            this.onconnect();
-        });
-        this.impl.addEventListener(flash.events.Event.CLOSE, function(e:Event) {
-            if (debug) trace('SocketFlash.close');
-            this.onclose();
-        });
-        this.impl.addEventListener(flash.events.IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) {
-            if (debug) trace('SocketFlash.io_error');
-            this.onerror();
-        });
-        this.impl.addEventListener(flash.events.ProgressEvent.SOCKET_DATA, function(e:ProgressEvent) {
-            var out = new ByteArray();
-            impl.readBytes(out, 0, impl.bytesAvailable);
-            out.position = 0;
-            if (debug) trace('SocketFlash.socket_data:' + out.toString());
-            this.ondata(Bytes.ofData(out));
-        });
-
+        this.impl.addEventListener(flash.events.Event.CONNECT, connectHandler);
+        this.impl.addEventListener(flash.events.Event.CLOSE, closeHandler);
+        this.impl.addEventListener(flash.events.IOErrorEvent.IO_ERROR, ioErrorHandler, false, 0, true);
+        this.impl.addEventListener(flash.events.ProgressEvent.SOCKET_DATA, socketDataHandler);
         this.impl.connect(host, port);
     }
 
+	function connectHandler(e:Event) {
+		if (debug) trace('${id} SocketFlash.connect');
+		this.onconnect();
+	}
+	
+	function closeHandler(e:Event) {
+		if (debug) trace('${id} SocketFlash.close');
+		this.onclose();
+	}
+
+	// TODO (DK) automatically call close or let the user do it?
+	function ioErrorHandler(e:IOErrorEvent) {
+		if (debug) trace('${id} SocketFlash.io_error');
+		this.onerror();
+	}
+
+	function socketDataHandler(e:ProgressEvent) {
+		var out = new ByteArray();
+		impl.readBytes(out, 0, impl.bytesAvailable);
+		out.position = 0;
+		if (debug) trace('${id} SocketFlash.socket_data:' + out.toString());
+		this.ondata(Bytes.ofData(out));
+	}
+	
     override public function close() {
+        impl.removeEventListener(flash.events.Event.CONNECT, connectHandler);
+        impl.removeEventListener(flash.events.Event.CLOSE, closeHandler);
+        impl.removeEventListener(flash.events.IOErrorEvent.IO_ERROR, ioErrorHandler);
+        impl.removeEventListener(flash.events.ProgressEvent.SOCKET_DATA, socketDataHandler);
         impl.close();
+		impl = null;
     }
 
     override public function send(data:Bytes) {
         var ba:ByteArray = data.getData();
         if (debug) {
-            trace('SocketFlash.send($ba) : ${ba.position} : ${ba.length}');
+            trace('${id} SocketFlash.send($ba) : ${ba.position} : ${ba.length}');
         }
         impl.writeBytes(ba);
         impl.flush();
